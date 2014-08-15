fc3ad7dc9d9c78fd193e18c1a55b965358c6de0e
Kazuya MIURA <miup.0627@gmail.com>
2014-08-15 19:51:49 +0900

    Initial Commit

---
+++ ScheduleManager/TKBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKBAppDelegate.h
+//  ScheduleManager
+//
+//  Created by kazuya on 8/15/14.
+//  Copyright (c) 2014 COINS Project AID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScheduleManager/TKBViewController.m
@@ -0,0 +1,29 @@
+//
+//  TKBViewController.m
+//  ScheduleManager
+//
+//  Created by kazuya on 8/15/14.
+//  Copyright (c) 2014 COINS Project AID. All rights reserved.
+//
+
+#import "TKBViewController.h"
+
+@interface TKBViewController ()
+
+@end
+
+@implementation TKBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
